8c7db2d85346a097e092fdfbd1dcc76a43d55521
execaus <execaus@gmail.com>
2023-03-07 20:54:20 +0500

    Добавляет сборку ts файлов и настраивает eslint

---
+++ src/constructor/constructor.ts
@@ -0,0 +1,14 @@
+class Constructor {
+	private readonly containerElement: HTMLElement;
+
+	constructor(container: HTMLElement) {
+		this.containerElement = container;
+		this.containerElement.textContent = 'asdasdasd';
+	}
+
+	public start = () => {
+
+	};
+}
+
+export default Constructor;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import Constructor from './constructor/constructor';
+
+const container = document.querySelector("#container") as HTMLElement
+const constructor = new Constructor(container);
